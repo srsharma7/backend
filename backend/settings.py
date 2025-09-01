5142d1f8e7ce2f27ea6711120b4d1ec5b7ff6cfc
srsharma7 <srsharma7177@gmail.com>
2025-09-01 11:09:04 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r^x%$u0pud=c6)w979hfb&)xphsn7z4&j%b%f3^(hzv3!b)qt7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['backend-sckx.onrender.com',]
+ALLOWED_HOSTS = ['backend-vc6l.onrender.com',]
 
 
 # Application definition
@@ -127,5 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
