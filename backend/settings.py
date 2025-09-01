4e6297a1bb34b880f49f358052fa5840c5791416
srsharma7 <srsharma7177@gmail.com>
2025-09-01 18:00:53 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS=[
-    
+    'https://https://feedback-bacec.web.app'
 ]
 ROOT_URLCONF = 'backend.urls'
 
@@ -134,4 +134,5 @@ MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
