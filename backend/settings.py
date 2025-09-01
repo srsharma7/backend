a40ac40fa80cc023996079adeb17f960fcc83f68
srsharma7 <srsharma7177@gmail.com>
2025-09-01 17:25:41 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r^x%$u0pud=c6)w979hfb&)xphsn7z4&j%b%f3^(hzv3!b)qt7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com',]
 
 
 # Application definition
@@ -134,3 +134,4 @@ MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
